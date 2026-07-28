fa6b8d97e523a7ca61010213d03c1a8413120489
ydah <t.yudai92@gmail.com>
2026-07-29 07:32:16 +0900

    Initial commit

---
+++ lib/box2d.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "box2d/version"
+
+module Box2d
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/box2d/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Box2d
+  VERSION = "0.1.0"
+end
